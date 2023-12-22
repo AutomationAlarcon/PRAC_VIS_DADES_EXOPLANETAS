185c6f17e5d174f59b7938329fb0e34711370efd
alfonsoalarcon <alfonsoalarcon@msn.com>
2023-12-22 20:56:13 +0100

    changes

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import pandas as pd
+from collections import Counter
+
+
+def new_label(df1):
+    df1.loc[(df1['mass'] >= 0.00001) & (df1['mass'] < 0.1), 'type'] = 'mercurian'
+    df1.loc[(df1['mass'] >= 0.1) & (df1['mass'] < 0.5), 'type'] = 'Subterran'
+    df1.loc[(df1['mass'] >= 0.5) & (df1['mass'] < 2), 'type'] = 'Earth'
+    df1.loc[(df1['mass'] >= 2) & (df1['mass'] < 10), 'type'] = 'Super-Earth'
+    df1.loc[(df1['mass'] >= 10) & (df1['mass'] < 50), 'type'] = 'Neptunian'
+    df1.loc[(df1['mass'] >= 50) & (df1['mass'] < 5000), 'type'] = 'Jovian'
+    df1.loc[(df1['mass'] >= 5000), 'type'] = 'Other'
+
 
 file_path = 'exoplanet.eu_catalog.csv'
 df = pd.read_csv(file_path, sep=",", skipinitialspace=True)
@@ -19,6 +31,16 @@ df1['detection_type'] = df1['detection_type'].astype('category')
 df1['molecules'] = df1['molecules'].astype('category')
 df1['mass_detection_type'] = df1['mass_detection_type'].astype('category')
 df1['star_name'] = df1['star_name'].astype('category')
+print(df1.dtypes)
+
+print(df['planet_status'].value_counts())
+print(df['detection_type'].value_counts())
+print(df['molecules'].value_counts())
+print(df['mass_detection_type'].value_counts())
+
+df2 = df.loc[df['planet_status'] == 'Confirmed']
+dis = df2['discovered'].tolist()
+print(Counter(dis))
 
 # Exoplanets classification
 #Subterran
@@ -33,10 +55,27 @@ df1['star_name'] = df1['star_name'].astype('category')
 #(Super-Earth/Mini-Neptunes)
 #3 — 10 ME or 1.6 — 2.5 RE
 
-print(df1.dtypes)
+#More categories in: https://www.sciencenews.org/article/kepler-telescope-doubles-its-count-known-exoplanets
 
-print(df['planet_status'].value_counts())
-print(df['detection_type'].value_counts())
-print(df['molecules'].value_counts())
-print(df['mass_detection_type'].value_counts())
+# Mars-sized
+# 0.5-0.7
+# Earth-sized
+# 0.7-1.2
+# Super-Earth-sized
+# 1.2-1.9
+# Sub-Neptune-sized
+# 1.9-3.1
+# Neptune-sized
+# 3.1-5.1
+# sub-Jupiter-sized
+# 5.1-8.3
+# Jupiter-sized
+# 8.3-13.7
+# Super-Jupiter-sized
+# 13.7-22.0
 
+new_label(df1)
+df3 = df1.loc[df['planet_status'] == 'Confirmed']
+df3['type'] = df3['type'].astype('category')
+t = df3['type'].tolist()
+print(Counter(t))
