e5786825555d6fe4bf3885d77228c604cd1dd965
alfonsoalarcon <alfonsoalarcon@msn.com>
2024-01-11 07:24:45 +0100

    changes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from collections import Counter
+from openpyxl.workbook import Workbook
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def set_data_column_mass(text1, text2):
@@ -115,6 +118,34 @@ def get_data_year_discovered():
     print(Counter(dis))
 
 
+def flatten(xss):
+    """
+    This method is to flat a list with various lists inside.
+
+    returns:
+        A list of elements.
+    """
+    return [x for xs in xss for x in xs]
+
+
+def get_molecules_values():
+    """
+    This method is to get a list with the molecules.
+
+    returns:
+        A list of molecules.
+    """
+    t = df1['molecules'].tolist()
+    t_values = [i for i in dict(Counter(t)).values()]
+    t_keys = list(dict(Counter(t)).keys())
+    print(t_values)
+    print(t_keys)
+    mol = []
+    for k in t_keys:
+        if str(k) != 'nan':
+            mol.append(str(k).split(','))
+    print(Counter(flatten(mol)))
+
 # source: https://exoplanet.eu/catalog/all_fields/
 file_path = 'exoplanet.eu_catalog.csv'
 df = pd.read_csv(file_path, sep=",", skipinitialspace=True)
@@ -127,7 +158,6 @@ df1 = df[['name', 'planet_status', 'mass',
 # Data are in Jupyter reference:
 #   - Jupyter mass is 317.83 times the mass of earth
 #   - Jupyter radius is 11.2 times the radius of earth
-
 df1['mass'] = df1['mass'] * 317.83
 df1['radius'] = df1['radius'] * 11.2
 
@@ -139,8 +169,24 @@ df1['mass_detection_type'] = df1['mass_detection_type'].astype('category')
 df1['star_name'] = df1['star_name'].astype('category')
 print(df1.dtypes)
 
-df1 = df1.loc[df1['planet_status'] == 'Confirmed']
 # get_value_counts()
+
+df1 = df1.loc[df1['planet_status'] == 'Confirmed']
+
+# Set the value of the radius=1.1 for Proxima Centauri b
+df1.loc[(df1['name'] == 'Proxima Centauri b'), 'radius'] = 1.1
+
 # get_data_year_discovered()
 # actions_mass_classification('mass', 'type_mass')
-# actions_radius_reduced_classification('radius', 'type_radius')
+# get_molecules_values()
+
+actions_radius_reduced_classification('radius', 'type_radius')
+df2 = df1[['name', 'mass',
+          'radius', 'orbital_period', 'type_radius']]
+filtered_df = df2[df2[['name', 'mass', 'radius', 'orbital_period', 'type_radius']].notnull().all(1)]
+
+
+sns.boxplot(filtered_df['orbital_period'])
+plt.show()
+filtered_df.to_excel('output.xlsx', index=False)
+print(filtered_df)
